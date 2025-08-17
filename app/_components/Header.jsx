ef8d35bde7ef7f41d5d529738644f8a987c03a34
Siddharth-Nmadev <namsiddharth19@gmail.com>
2025-08-18 01:58:39 +0530

    store user info

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,18 +1,40 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 import Authentication from "./Authentication";
+import { useAuthContext } from "../provider";
+import Link from "next/link";
 
 export const Header = () => {
+  const { user } = useAuthContext();
+
   return (
     <div className="flex justify-between p-4">
       <div className="flex items-center gap-3">
         <Image src={"/logo.svg"} alt="logo" width={30} height={30} />
         <h2 className="text-2xl font-bold"> Video Gen</h2>
       </div>
-      <Authentication>
-        <Button className="cursor-pointer">Get Started</Button>
-      </Authentication>
+      {!user ? (
+        <Authentication>
+          <Button className="cursor-pointer">Get Started</Button>
+        </Authentication>
+      ) : (
+        <div className="flex items-center gap-3">
+          <Link href={"/dashborad"}>
+            {" "}
+            <Button className="cursor-pointer">Dashboard</Button>
+          </Link>
+
+          <Image
+            src={user?.photoURL}
+            alt="user Image"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+        </div>
+      )}
     </div>
   );
 };
--- app/provider.js
+++ app/provider.js
@@ -1,27 +1,39 @@
-"use client";  
+"use client";
 // when refresh the page this provider will call all time
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { onAuthStateChanged } from "firebase/auth";
-export const Provider = ({ children }) => {
+import { AuthContext } from "./AuthContext";
+import { auth } from "@/configs/firebaseConfig";
 
-  useEffect(()=>{
-    const unSubscribe = onAuthStateChanged(auth,(user)=>{  // it check when you refresh , you login or not
+export const Provider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      // it check when you refresh , you login or not
       console.log(user);
-    })
-    return ()=> unSubscribe()
-  },[])
+    });
+    return () => unSubscribe();
+  }, []);
 
   return (
     <div>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </NextThemesProvider>
+      <AuthContext.Provider value={{ user }}>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </NextThemesProvider>
+      </AuthContext.Provider>
     </div>
   );
+};
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  return context;
 };
